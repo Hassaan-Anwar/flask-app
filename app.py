6ab3bbaee09b171ca507a5eaaf23b83bdbf8b026
Hassaan-Anwar <hassaan.anwar20052@gmail.com>
2025-08-28 18:22:10 +0500

    Update: Added new page

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
 
 def add(a, b):
@@ -7,7 +6,19 @@ def add(a, b):
 
 @app.route('/')
 def home():
-    return 'Flask app is running!'
+    return render_template("index.html")
+
+
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        name="Hassaan",
+        course="BS Computer Science MLOPS",
+        section="CS-K",
+        roll_number="22I-0890"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
